f52a2044d9203995d373674abba5802313367ae6
Wilfried Klaas <w.klaas@gmx.de>
2022-06-04 07:55:06 +0200

    WIP: smaller in sourceode

--- src/components/siminputs.vue
+++ src/components/siminputs.vue
@@ -26,30 +26,22 @@
             <div class="col">
                 <label for="ADC1">ADC 1</label><br />
                 <Knob id="ADC1" name="ADC1" v-model="adc1i" :min="0" :max="100" valueTemplate="{value}%" :size="70" />
-                <font size="1rem">
-                    {{ad1hex}}
-                </font> 
+                <font size="1rem">{{ad1hex}}</font> 
             </div>
             <div v-if="['ArduinoTPS', 'Microbit', 'ESP32'].includes(this.selectedHardware)" class="col">
                 <label for="ADC2">ADC 2</label><br />
                 <Knob id="ADC2" name="ADC2" v-model="adc2i" :min="0" :max="100" valueTemplate="{value}%" :size="70" />
-                <font size="1rem">
-                {{ad2hex}}
-                </font>
+                <font size="1rem">{{ad2hex}}</font>
             </div>
             <div v-if="['ArduinoTPS', 'Microbit', 'ESP32'].includes(this.selectedHardware)" class="col">
                 <label for="RC1">RC 1</label><br />
                 <Knob id="RC1" name="RC1" v-model="rc1i" :min="0" :max="180" valueTemplate="{value}°" :size="70" />
-                <font size="1rem">
-                {{rc1hex}}
-                </font>
+                <font size="1rem">{{rc1hex}}</font>
             </div>
             <div v-if="['ArduinoTPS', 'Microbit', 'ESP32'].includes(this.selectedHardware)" class="col">
                 <label for="RC2">RC 2</label><br />
                 <Knob id="RC2" name="RC2" v-model="rc2i" :min="0" :max="180" valueTemplate="{value}°" :size="70" />
-                <font size="1rem">
-                {{rc2hex}}
-                </font>
+                <font size="1rem">{{rc2hex}}</font>
             </div>
         </div>
         <div class="grid">
