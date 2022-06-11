422040d7f4e2ab1a4a9045d4dd52084fa69c810a
Willie <w.klaas@gmx.de>
2022-06-11 11:58:45 +0200

    WIP: adding zip command, because powershell version doesn't woirk with aws

--- deploy.cmd
+++ deploy.cmd
@@ -4,4 +4,4 @@
 call npm run build
 del /s /q \\192.168.178.14\Docker\php_base\hosting\vtps\*
 xcopy /s /q .\dist\* \\192.168.178.14\Docker\php_base\hosting\vtps
-powershell -command "Compress-Archive -Path .\dist -DestinationPath .\dist\dist.zip"
+".\3rd party\zip\7z.exe" a  .\dist\dist.zip .\dist\*
