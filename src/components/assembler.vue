af733da92516ef5a56d912ba352c8eedf1ee76ba
Willie <w.klaas@gmx.de>
2022-07-26 21:14:32 +0200

    Bug: using ascii doc highlighter

--- src/components/assembler.vue
+++ src/components/assembler.vue
@@ -76,7 +76,7 @@ import 'vue-prism-editor/dist/prismeditor.min.css'; // import the styles somewhe
 import { highlight, languages } from 'prismjs/components/prism-core';
 import 'prismjs/components/prism-clike';
 import 'prismjs/components/prism-javascript';
-import 'prismjs/components/prism-asm6502';
+import 'prismjs/components/prism-asciidoc';
 import 'prismjs/themes/prism-tomorrow.css'; // import syntax highlighting styles
 
 export default {
@@ -151,7 +151,7 @@ export default {
         highlighter(code) {
             console.log(code)
             // return code; 
-            return highlight(code, languages.asm6502); // languages.<insert language> to return html with markup
+            return highlight(code, languages.asciidoc); // languages.<insert language> to return html with markup
         },
         saveFile() {
             if (this.tabIndex == 0) {
